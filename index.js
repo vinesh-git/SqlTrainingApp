5697c3895a3956274c9a74634a6dbdc6e229c8ca
vinesh-git <75483904+vinesh-git@users.noreply.github.com>
2024-01-18 06:32:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function record() {
+    var topicid = document.getElementById('id').value;
+    var topic_to_be_covered = document.getElementById('topic_to_be_covered').value;
+    var what_all_was_covered = document.getElementById('what_all_was_covered').value;
+    var date = document.getElementById('date').value;
+    var checkedvalues = document.querySelector('input[name="tasks"]:checked').value;
+    var questions = document.getElementById('questions').value;
+    var challenges = document.getElementById('challenges').value;
+
+    var obj = JSON.stringify({
+        id : topicid,
+        Topic_To_Be_Covered : topic_to_be_covered,
+        What_All_Was_Covered : what_all_was_covered,
+        Date : date,
+        Tasks_Given : checkedvalues,
+        Any_Questions : questions,
+        Challenges_faced : challenges
+    })
+
+    var current = new Date();
+    localStorage.setItem(current,obj);
+
+}
+
+
+
+
